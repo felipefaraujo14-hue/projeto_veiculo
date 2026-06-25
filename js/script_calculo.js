4492cdcf0114b4c40df6159250ab8f7ef13b02a7
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-06-25 16:37:06 -0300

    html atualizado e concluido

--- js/script_calculo.js
+++ js/script_calculo.js
@@ -1,8 +1,42 @@
-function calcularIPVA(valorVeiculo) {
+function calcularIPVA(valorVeiculo, combustivel) {
+
+    if (combustivel === "Gasolina") {
+        return valorVeiculo * 0.04;
+    }
+
+    if (combustivel === "Etanol") {
+        return valorVeiculo * 0.03;
+    }
+
+    if (combustivel === "GNV") {
+        return valorVeiculo * 0.02;
+    }
+
+    if (combustivel === "Diesel") {
+        return valorVeiculo * 0.05;
+    }
+
     return valorVeiculo * 0.04;
 }
 
-function calcularSeguro(valorVeiculo) {
+function calcularSeguro(valorVeiculo, combustivel) {
+
+    if (combustivel === "Gasolina") {
+        return valorVeiculo * 0.10;
+    }
+
+    if (combustivel === "Etanol") {
+        return valorVeiculo * 0.08;
+    }
+
+    if (combustivel === "GNV") {
+        return valorVeiculo * 0.06;
+    }
+
+    if (combustivel === "Diesel") {
+        return valorVeiculo * 0.12;
+    }
+
     return valorVeiculo * 0.10;
 }
 
