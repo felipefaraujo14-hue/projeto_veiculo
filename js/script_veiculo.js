c701ebed114b5957f8e4dc9418f34e38371ddc21
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-06-25 14:25:34 -0300

    script-veiculo corrigido e terminado

---
+++ js/script_veiculo.js
@@ -0,0 +1,83 @@
+const form = document.getElementById("formVeiculo");
+const lista = document.getElementById("listaVeiculos");
+
+const veiculos = [];
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const marca = document.getElementById("marca").value;
+    const modelo = document.getElementById("modelo").value;
+    const placa = document.getElementById("placa").value;
+    const ano = Number(document.getElementById("ano").value);
+    const valor = Number(document.getElementById("valor").value);
+
+    const combustivel = document.querySelector(
+        'input[name="combustivel"]:checked'
+    ).value;
+
+    const veiculo = {
+        marca,
+        modelo,
+        placa,
+        ano,
+        valor,
+        combustivel
+    };
+
+    veiculos.push(veiculo);
+
+    exibirVeiculos();
+
+    form.reset();
+});
+
+function exibirVeiculos() {
+
+    lista.innerHTML = "";
+
+    veiculos.forEach((veiculo) => {
+
+        const idade =
+            new Date().getFullYear() - veiculo.ano;
+
+        const ipva =
+            calcularIPVA(veiculo.valor);
+
+        const seguro =
+            calcularSeguro(veiculo.valor);
+
+        const licenciamento =
+            calcularLicenciamento();
+
+        const valorFinal =
+            ipva + seguro + licenciamento;
+
+        lista.innerHTML += `
+            <div class="card">
+
+                <p><strong>Marca:</strong> ${veiculo.marca}</p>
+
+                <p><strong>Modelo:</strong> ${veiculo.modelo}</p>
+
+                <p><strong>Placa:</strong> ${veiculo.placa}</p>
+
+                <p><strong>Idade:</strong> ${idade} anos</p>
+
+                <p><strong>Seguro:</strong>
+                R$ ${seguro.toFixed(2)}</p>
+
+                <p><strong>IPVA:</strong>
+                R$ ${ipva.toFixed(2)}</p>
+
+                <p><strong>Licenciamento:</strong>
+                R$ ${licenciamento.toFixed(2)}</p>
+
+                <p><strong>Valor Final:</strong>
+                R$ ${valorFinal.toFixed(2)}</p>
+
+            </div>
+        `;
+    });
+}
