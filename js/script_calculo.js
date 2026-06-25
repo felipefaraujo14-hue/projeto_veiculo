4603504ae35513b257df02be4e7d81230ce7c06e
Felipe <felipe.f.araujo14@aluno.senai.br>
2026-06-25 14:07:01 -0300

    feat: adicionar cálculo de IPVA e seguro

---
+++ js/script_calculo.js
@@ -0,0 +1,11 @@
+export function calcularIPVA(valorVeiculo) {
+    return valorVeiculo * 0.04;
+}
+
+export function calcularSeguto(valorVeiculo) {
+    return valorVeiculo * 0.02;
+}
+
+export function calcularLicenciamento() {
+    return 150;
+}
